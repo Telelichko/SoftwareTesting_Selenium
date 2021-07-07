a0770e53404d67eb0b5c5c639526b8605353cfe8
Telelichko <52406628+Telelichko@users.noreply.github.com>
2021-07-07 22:52:45 +0700

    First sketches in exercise 11

---
+++ lesson6_11.py
@@ -0,0 +1,46 @@
+import random
+
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture()
+def driver(request):
+    wd = webdriver.Chrome()
+    wd.implicitly_wait(10)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_create_account(driver):
+    main_page_url = 'http://localhost/litecart/'
+
+    driver.get(main_page_url)
+
+    link_registration = driver.find_element_by_link_text('New customers click here')
+    link_registration.click()
+
+    inputs = driver.find_elements_by_tag_name('input')
+
+    for input in inputs:
+        parent_input_text = input.find_element_by_xpath('//parent::td[1]').text
+
+        if parent_input_text == 'Postcode':
+            input_text = '012345'
+        elif parent_input_text == 'Email':
+            input_text = 'mail@mail.com'
+        elif parent_input_text == 'Phone':
+            input_text = '799999999'
+        elif 'Password' in parent_input_text:
+            input_text = 'Password'
+        else:
+            # TODO: !!! 1 проблема - "ElementNotInteractableException: Message: element not interactable" -
+            # TODO: Поле ввода под "Tax ID" не предназначено для взаимодействий !!!
+            input_text = parent_input_text
+
+        # TODO: !!! 2 проблема - Как быть с полем для "CAPTCHA" ???
+
+
+        input_text += str(random.randint(100, 999))
+        input.send_keys(input_text)
+
