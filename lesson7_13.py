2bd20769461e95c523a08b2a3113270fa8cf6de5
Telelichko <52406628+Telelichko@users.noreply.github.com>
2021-07-14 22:30:24 +0700

    Waiting for page deletion.

--- lesson7_13.py
+++ lesson7_13.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -7,15 +9,17 @@ from selenium.webdriver.common.by import By
 @pytest.fixture()
 def driver(request):
     wd = webdriver.Chrome()
-    wd.implicitly_wait(5)
+    wd.implicitly_wait(2)
     request.addfinalizer(wd.quit)
     return wd
 
 #region TESTS_METHODS_REGION
 
-def test_add_products_to_cart(driver):
+def test_add_delete_cart_products(driver):
     for i in range(3):
         add_product_to_cart(driver)
+
+    delete_products_in_cart(driver)
 
 #endregion
 
@@ -40,6 +44,22 @@ def add_product_to_cart(driver):
 
     WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element(
         (By.XPATH, '//a[@class="content" and contains(., "Cart")]//span[@class="quantity"]'), f'{int(count_products_in_cart) + 1}'))
+
+def delete_products_in_cart(driver):
+    link_cart = driver.find_element_by_xpath('//a[contains(text(), "Checkout")]')
+    link_cart.click()
+
+    list_products = driver.find_elements_by_xpath('//ul[@class="items"]//li')
+    product_count = len(list_products)
+
+    for i in range(product_count):
+        button_remove = driver.find_element_by_xpath('//button[text()="Remove"]')
+        WebDriverWait(driver, 10).until(EC.visibility_of(button_remove))
+        button_remove.click()
+
+        WebDriverWait(driver, 10).until(EC.invisibility_of_element(button_remove))
+
+    go_to_main_page(driver)
 
 def check_and_select_small_product_size(driver):
     lists_product_size = driver.find_elements_by_xpath(f'//div[@class="content"]//tr[contains(., "Size")]//select')
