c010d959288d3cd7ce99bacc1679f172c63170ff
Telelichko <52406628+Telelichko@users.noreply.github.com>
2021-07-30 19:29:11 +0700

    Added comment with settings in VirtualBox

--- lesson9_15.py
+++ lesson9_15.py
@@ -42,6 +42,11 @@ def test_check_course_page(driver):
 #region ACTIONS_METHODS_REGION
 
 def select_browser(browser, request):
+    # In virtualBox: https://coderoad.ru/43610397/%D0%9D%D0%B5-%D1%83%D0%B4%D0%B0%D0%B5%D1%82%D1%81%D1%8F-%D0%B4%D0%BE%D1%81%D1%82%D0%B8%D1%87%D1%8C-%D1%83%D0%B7%D0%BB%D0%B0-%D0%BD%D0%B0-%D0%B2%D0%B8%D1%80%D1%82%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B9-%D0%BC%D0%B0%D1%88%D0%B8%D0%BD%D0%B5-%D1%81-%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D1%8C%D1%8E-%D1%81%D0%B5%D1%82%D0%BA%D0%B8-Selenium
+    # Перейдите в Настройки -> Сеть
+    # 1. Прилагается к должно быть 'Bridged Adapter'
+    # 2. Разверните 'Advanced' -> Неразборчивый режим должен быть 'Allow All'
+
     url = 'Here should be your url. Like: http://XXX.XXX.XXX.XXX .'
     if browser == Browser.CHROME:
         wd = webdriver.Chrome()
